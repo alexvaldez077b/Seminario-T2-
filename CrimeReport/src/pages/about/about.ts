c2bb5b4ae6ac8fe95d9d3702910e40d4fedb3d97
alejandro valdez campos <alexvaldez077b@gmail.com>
2017-09-06 14:57:05 -0600

    Google maps
    Google maps visualization and move maker

--- CrimeReport/src/pages/about/about.ts
+++ CrimeReport/src/pages/about/about.ts
@@ -21,10 +21,24 @@ export class AboutPage {
 
   }
 
+  addInfoWindow(marker, content){
 
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
 
   ngAfterViewInit() {
     this.geolocation.getCurrentPosition().then((resp) => {
+      this.position = { 'lat': resp.coords.latitude,'lng': resp.coords.longitude};
+
+      console.log(this.position);
+
       this.loadMap(resp)
      // resp.coords.latitude
      // resp.coords.longitude
@@ -37,60 +51,8 @@ export class AboutPage {
 
 	}
 
-  loadMap(resp){
+  public  addMarker(){
 
-    let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
-
-    let mapOptions = {
-      center: latLng,
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-    console.log(this.map);
-
-   this.map.addListener('click', function(e) {
-
-
-          this.addMarker();
-          /*console.log(e.ea.x + ' -- ' + e.ea.y);
-          var marker = new google.maps.Marker({
-            map: this.map,
-            animation: google.maps.Animation.DROP,
-            position: {'lat': e.ea.x, 'lng': e.ea.y }
-          });
-
-          let content = "<h4>Information!</h4>";
-
-          let infoWindow = new google.maps.InfoWindow({
-            content: content
-          });
-
-          google.maps.event.addListener(marker, 'click', () => {
-            infoWindow.open(this.map, marker);
-          });*/
-      });
-
-      
-
-  }
-
-
-
-
-  addMarker(){
-
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.position = { 'lat': resp.coords.latitude,'lng': resp.coords.longitude};
-
-      console.log(this.position);
-
-
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
 
     let marker = new google.maps.Marker({
       map: this.map,
@@ -99,24 +61,77 @@ export class AboutPage {
       draggable: true
     });
 
-    let content = "<h4>Information!</h4>";
+    let content = "<h4>Location </h4>";
 
     this.addInfoWindow(marker, content);
 
+
+
+    }
+
+  loadMap(resp){
+
+
+    /* Data points defined as a mixture of WeightedLocation and LatLng objects */
+    var heatMapData = [
+
+      new google.maps.LatLng(31.647153699999, -106.4455448),
+
+      new google.maps.LatLng(31.647153699999992, -106.4455448),
+      new google.maps.LatLng(31.647153699999993, -106.4455450),
+      new google.maps.LatLng(31.647153699999991, -106.4455453),
+      new google.maps.LatLng(31.647153699999990, -106.4455441),
+      new google.maps.LatLng(31.647153699999990, -106.4455485),
+      new google.maps.LatLng(31.647153699999985, -106.4455415),
+      new google.maps.LatLng(31.647153699999995, -106.4455448),
+      new google.maps.LatLng(31.647153699999970, -106.4455448),
+      new google.maps.LatLng(31.647153699999990, -106.4455448),
+
+
+
+    ];
+
+
+
+    let latLng = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    var heatmap = new google.maps.visualization.HeatmapLayer({
+      data: heatMapData
+    });
+
+    heatmap.setMap(this.map);
+
+
+
+
+
+    /*
+    this.addMarker();
+    this.addMarker();
+    this.addMarker();
+    this.addMarker();
+    this.addMarker();
+
+    */
+
+
   }
 
 
 
-  addInfoWindow(marker, content){
 
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
 
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
 
-  }
+
+
+
 
 }
